9c6b1d31036ff0f53a738e66e78ee993700a4454
Nikolas.Dev <nikolasdssantana@gmail.com>
2021-08-07 17:43:50 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -70,12 +70,14 @@ cards.forEach(card => {
   });
 });
 
+
 document.querySelector(".close-modal").addEventListener("click", () => {
   modalOverlay.classList.remove("active");
   modal.classList.remove("active");
   modal.querySelector("iframe").src = ``;
   document.querySelector("body").style.overflow = "initial";
 });
+
 
 let show = true;
 
